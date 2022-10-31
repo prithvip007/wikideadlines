4c86851ecab279adddc2613eac5fc0167c23f80f
Codeinit <codeinit@gadgetzones-Mac-mini.local>
2022-10-31 15:48:27 +0530

    roles check

--- app/app/Http/Controllers/Dashboard/UserController.php
+++ app/app/Http/Controllers/Dashboard/UserController.php
@@ -14,7 +14,9 @@ class UserController extends Controller
         $id = (int) $request->id;
 
         $user = User::withCount('calculations')->findOrFail($id);
-
+        echo "<pre>";
+        print_r($user->roles);
+        exit("fnkj");
         return view('dashboard.pages.user', [ 'user' => $user ]);
     }
 
