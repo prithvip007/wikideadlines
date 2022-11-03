e45a40fd36bdd889d4e6f55b94b02017af3cb11c
Anju Saini <anjusaini@Anju-ka-Mac-mini.local>
2022-11-03 10:16:46 +0530

    pricingf

--- app/resources/views/pricing/index.blade.php
+++ app/resources/views/pricing/index.blade.php
@@ -21,7 +21,7 @@
             </div>
             <div class="col-12 flex-grow-1 flex-basis-auto">
               {{ $publishableKey}} 
-              {{ $plans}}
+              {{ json_encode($plans)}}
               <p class="text-muted py-2">
                 <i class="fa fa-asterisk" aria-hidden="true" style="position: relative;top: -7px;font-size: 6px;"></i>
                 You get one free search every 30 days - just create your account.
