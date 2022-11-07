b1d5d785e71afb9319ae5b0eaf1207f143879398
Anju Saini <anjusaini@Anju-ka-Mac-mini.local>
2022-11-07 17:26:49 +0530

    how should it be

--- app/resources/js/components/RuleForm.vue
+++ app/resources/js/components/RuleForm.vue
@@ -251,7 +251,7 @@
                     </div>
                     <div class="form-group">
                             <label class="font-weight-bold">
-                                How should it be filtered change hear?
+                                How should it be filtered?
                             </label>
                                 <div
                                     v-for="item, index in [
