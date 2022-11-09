f57c61752cbde859cd3180fb72f6ba7bc8e832aa
Anju Saini <anjusaini@Anju-ka-Mac-mini.local>
2022-11-09 13:31:20 +0530

    composer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -168,8 +168,8 @@ class ComposerStaticInit16791a02c9a7bf3296d21de9fbb56825
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
@@ -193,9 +193,9 @@ class ComposerStaticInit16791a02c9a7bf3296d21de9fbb56825
         ),
         'Illuminate\\Support\\' => 
         array (
-            0 => __DIR__ . '/..' . '/illuminate/conditionable',
-            1 => __DIR__ . '/..' . '/illuminate/macroable',
-            2 => __DIR__ . '/..' . '/illuminate/collections',
+            0 => __DIR__ . '/..' . '/illuminate/collections',
+            1 => __DIR__ . '/..' . '/illuminate/conditionable',
+            2 => __DIR__ . '/..' . '/illuminate/macroable',
             3 => __DIR__ . '/..' . '/illuminate/support',
         ),
         'Illuminate\\Session\\' => 
