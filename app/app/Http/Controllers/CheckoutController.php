d5c2905261c8c308c1998c4dc14bea0a6ebb327b
Anju Saini <anjusaini@Anju-ka-Mac-mini.local>
2022-11-11 15:36:21 +0530

    change after compair

--- app/app/Http/Controllers/CheckoutController.php
+++ app/app/Http/Controllers/CheckoutController.php
@@ -29,9 +29,7 @@ class CheckoutController extends Controller
         }
 
         $plan = StripePlan::where('interval', $request->input('billing_plan'))->first();
-        echo "<pre>";
-        print_r($plan);
-      
+       
 
         $checkout_session = \Stripe\Checkout\Session::create([  
             'mode' => 'subscription',
