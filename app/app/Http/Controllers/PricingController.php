02550775af78ee50f093e16c5d971ae849a43ec8
Anju Saini <anjusaini@Anju-ka-Mac-mini.local>
2022-11-14 16:19:26 +0530

    strip

--- app/app/Http/Controllers/PricingController.php
+++ app/app/Http/Controllers/PricingController.php
@@ -8,37 +8,32 @@ use App\Models\StripePlan;
 
 class PricingController extends Controller
 {
-    // public function index()
-    // {
-    //     $user = Auth::guard()->user();
-      
-    //     $subscription = $user->subscription();
-    //     $cancelled = $subscription === null ? false : $subscription->cancelled();
-
-    //     $stripePlans = StripePlan::all();
-
-    //     $plans = [];
-
-    //     foreach ($stripePlans as $stripePlan) {
-
-    //         $plans[] = [
-    //             'price' => $stripePlan->price_amount,
-    //             'active' => $user->subscribedToPlan($stripePlan->price_id),
-    //             'interval' => $stripePlan->interval,
-    //             'canceled' => $cancelled,
-    //         ];
-           
-    //     }
-    //     $publishableKey = config('app.stripe.key');
-    //     return view('pricing.index', compact('plans', 'publishableKey'));
-    // }
     public function index()
     {
-        // echo "reach hear ";
-        // die;
-        return view('pricing.indexprice');
+        $user = Auth::guard()->user();
+      
+        $subscription = $user->subscription();
+        $cancelled = $subscription === null ? false : $subscription->cancelled();
+
+        $stripePlans = StripePlan::all();
+
+        $plans = [];
+
+        foreach ($stripePlans as $stripePlan) {
+
+            $plans[] = [
+                'price' => $stripePlan->price_amount,
+                'active' => $user->subscribedToPlan($stripePlan->price_id),
+                'interval' => $stripePlan->interval,
+                'canceled' => $cancelled,
+            ];
+           
+        }
+
+        $publishableKey = config('app.stripe.key');
        
+
+
+        return view('pricing.index', compact('plans', 'publishableKey'));
     }
-    
-    
 }
--- app/routes/web.php
+++ app/routes/web.php
@@ -70,9 +70,6 @@ Route::middleware(['auth'])->group(function () {
 
         Route::get('/pricing', 'PricingController@index')
             ->name('pricing');
-            
-            // Route::get('/pricingnow', 'PricingController@pricingnow')
-            // ->name('pricingnow');
 
         // TODO: remove
         Route::post('/beta-test', 'CalculatorController@calculate');
