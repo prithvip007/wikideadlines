3e3b9591bf6eef480ddc25d0b5fb2e173e885b4d
Anju Saini <anjusaini@Anju-ka-Mac-mini.local>
2022-11-14 16:05:32 +0530

    view page

--- app/resources/views/pricing/indexprice.blade.php
+++ app/resources/views/pricing/indexprice.blade.php
@@ -5,7 +5,34 @@
 @endsection
 
 @section('content')
+<style>
+.top{
+display:flex;
+flex-direction:row;
 
+}
+  </style>
 <h1> i am hear</h1>
    
+<div class="container top">
+   <div class="box">
+      <h3><h3>Heading part</h3> 
+      <p> this is main paregraph </p>
+      <p>this is footer <buttton>select </buttton></p>
+    </div> 
+
+    <div class="box">
+        <h3><h3>Heading part</h3> 
+        <p> this is main paregraph </p>
+        <p>this is footer <buttton>select </buttton></p>
+    </div>
+
+    <div class="box">
+        <h3><h3>Heading part</h3> 
+        <p> this is main paregraph </p>
+        <p>this is footer <buttton>select </buttton></p>
+    </div>
+   
+</div>
+
 @endsection
