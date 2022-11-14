239aeb69c46a04018ff3203fe8d0791e8a894686
Anju Saini <anjusaini@Anju-ka-Mac-mini.local>
2022-11-14 11:03:08 +0530

    change

--- app/app/Http/Controllers/CalculatorController.php
+++ app/app/Http/Controllers/CalculatorController.php
@@ -131,11 +131,7 @@ class CalculatorController extends Controller
 
     public function calculate(Request $request)
     {
-<<<<<<< HEAD
-        $document_type_id = (int) $request->input('document_type_id');
-=======
-       $do3ument_type_id = (int) $request->input('document_type_id');
->>>>>>> parent of a401b03d... compair  check
+       $douument_type_id = (int) $request->input('document_type_id');
         $state_id = (int) $request->input('state_id');
         $county_id = (int) $request->input('county_id');
 
