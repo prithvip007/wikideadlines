f37de3ff8fcd1398a42973caecef863489beb5af
Anju Saini <anjusaini@Anju-ka-Mac-mini.local>
2022-11-22 13:58:53 +0530

    change

--- app/vendor/phpunit/phpunit/src/Framework/TestResult.php
+++ app/vendor/phpunit/phpunit/src/Framework/TestResult.php
@@ -637,7 +637,7 @@ final class TestResult implements Countable
         $error      = false;
         $failure    = false;
         $warning    = false;
-        $incomplete = true;
+        $incomplete = false;
         $risky      = false;
         $skipped    = false;
 
