c99f4818253b32f33dd80f0421dce9a1a9d03759
Anju Saini <anjusaini@Anju-ka-Mac-mini.local>
2022-11-22 17:22:53 +0530

    last fix

--- app/app/Http/Controllers/WebhookController.php
+++ app/app/Http/Controllers/WebhookController.php
@@ -52,9 +52,7 @@ class WebhookController extends CashierController
        
         if ($user) {
             $data = $payload['data']['object'];
-           echo "<pre>";
-           print_r($data);
-
+           
             if (! $user->subscriptions->contains('stripe_id', $data['id'])) {
                 if (isset($data['trial_end'])) {
                     $trialEndsAt = Carbon::createFromTimestamp($data['trial_end']);
