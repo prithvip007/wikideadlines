26cb279cfa34283e43f2e86088af59bccd64b9f7
Anju Saini <anjusaini@Anju-ka-Mac-mini.local>
2022-11-25 16:40:21 +0530

    check

--- app/app/Http/Controllers/CalculatorController.php
+++ app/app/Http/Controllers/CalculatorController.php
@@ -131,7 +131,9 @@ class CalculatorController extends Controller
     }
 
     public function calculate(Request $request)
-    {
+    { 
+        echo "ftoo";
+        die;
         
        $document_type_id = (int) $request->input('document_type_id');
         $state_id = (int) $request->input('state_id');
@@ -339,6 +341,8 @@ class CalculatorController extends Controller
 
     public function calculation(string $key)
     {
+        echo "dfgd";
+        die;
         $calculation = Calculation::findByKeyOrFail($key);
 
         $ruleInterview = new RuleInterview();
