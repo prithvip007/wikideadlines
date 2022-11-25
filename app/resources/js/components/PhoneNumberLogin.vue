60d78aeb4abd16d11f44b21adfa2c2c8fadc8f1d
gadgetzone <gadgetzone@gadgetzones-Mac-mini.local>
2022-11-25 18:21:30 +0530

    changes

--- app/resources/js/components/PhoneNumberLogin.vue
+++ app/resources/js/components/PhoneNumberLogin.vue
@@ -7,14 +7,14 @@
     <div style="overflow:hidden;" class="card card-m">
         <h5 v-if="currentStep === PHONE_STEP && mode === 'logging'" class="card-header text-center bg-white">Log in with your phone number</h5>
         <h5 v-if="currentStep === PHONE_STEP && mode === 'signup'" class="card-header text-center bg-white">Sign up with your phone number</h5>
-        <modal-header-primary v-if="currentStep === PHONE_STEP && mode === 'logging'" :lines="['Phone number change']"></modal-header-primary>
+        <modal-header-primary v-if="currentStep === PHONE_STEP && mode === 'update'" :lines="['Phone number change']"></modal-header-primary>
         <h5 v-else-if="currentStep === CODE_STEP" class="card-header text-center bg-white">Verify your phone number</h5>
         <div class="card-body">
             <form @submit.prevent="sendForm" class="p-0 px-md-5 py-md-2">
                 <div v-if="currentStep === PHONE_STEP">
                     <div class="form-group d-flex align-items-center flex-column">
                         <div v-if="mode === 'logging'" class="text-center mb-3">Please enter your phone number<br>to log in to WikiDeadlines</div>
-                        <div v-if="mode === 'logging'"  class="text-center mb-3">Please enter a new phone number<br>that you will use to log in to WikiDeadlines</div>
+                        <div v-if="mode === 'update'"  class="text-center mb-3">Please enter a new phone number<br>that you will use to log in to WikiDeadlines</div>
                         <div v-if="mode === 'signup'" class="text-center mb-3">Please enter your phone number<br>to sign up to WikiDeadlines</div>
                             <vue-phone-number-input border-radius="10"  class="w-100" size="sm" :no-example="true" autofocus v-on:update="handlePhoneNumberUpdate" autocomplete="tel" required v-model="phone"></vue-phone-number-input>
                         <div v-if="isPhoneNumberValid === false" class="invalid-feedback">
