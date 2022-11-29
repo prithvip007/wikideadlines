df1c967821accecc272a6e79d0b38c44fc049ccc
gadgetzone <gadgetzone@gadgetzones-Mac-mini.local>
2022-11-29 11:58:34 +0530

    changes

--- app/config/app.php
+++ app/config/app.php
@@ -164,7 +164,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Spatie\GoogleCalendar\GoogleCalendarServiceProvider::class,
+        // Spatie\GoogleCalendar\GoogleCalendarServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -233,7 +233,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'GoogleCalendar' => Spatie\GoogleCalendar\GoogleCalendarFacade::class,
+        // 'GoogleCalendar' => Spatie\GoogleCalendar\GoogleCalendarFacade::class,
 
     ],
 
