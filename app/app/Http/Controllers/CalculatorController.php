82dcb01c90d47504fee1b24b04f4233a761c70d7
gadgetzone <gadgetzone@gadgetzones-Mac-mini.local>
2022-11-29 11:36:28 +0530

    changes

--- app/app/Http/Controllers/CalculatorController.php
+++ app/app/Http/Controllers/CalculatorController.php
@@ -149,7 +149,7 @@ class CalculatorController extends Controller
     {
         // $event = new Event;
         // // 'A new event';
-        // $event->name = $request->input('name');
+        // $event->name = "An Event Added";
         // $event->description = 'Event description';
         // $event->startDateTime = Carbon\Carbon::now();
         // $event->endDateTime = Carbon\Carbon::now()->addHour();
--- app/config/app.php
+++ app/config/app.php
@@ -164,6 +164,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Spatie\GoogleCalendar\GoogleCalendarServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -232,6 +233,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'GoogleCalendar' => Spatie\GoogleCalendar\GoogleCalendarFacade::class,
 
     ],
 
